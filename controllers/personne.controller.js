f37541a9b525ca08a339542c30074af3cf15c367
PaulBesson <paul.besson@hotmail.com>
2025-09-04 14:37:08 +0200

    cors + basic-auth

--- controllers/personne.controller.js
+++ controllers/personne.controller.js
@@ -39,31 +39,30 @@ const showOne = async (req, res, next) => {
 		.sendStatus(404);
 }
 
-const add = async (req, res, next) => {
+const add = (req, res, next) => {
 
-    personneSchema
-        .validate(req.body, { abortEarly: false })
-        .then(async () => {
-            const p = await personneRepository.save(req.body)
-            if (p) {
-                return res
-                    .status(201)
-                    .json(p)
-            }
-        })
-        .catch(async err => {
-            console.log(err);
-            return res
-                .sendStatus(500)
-        })
+	personneSchema
+		.validate(req.body, { abortEarly: false })
+		.then(async () => {
+			const p = await personneRepository.save(req.body)
+			if (p) {
+				return res
+					.status(201)
+					.json(p)
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			return res
+				.sendStatus(500)
+		})
 }
 
 const remove = async (req, res, next) => {
-	const id = req.params.id
+	const id = req.params.id;
 	const personne = personneRepository.findById(id);
-	if (personne)
-	{
-		await personneRepository.deleteById(id)
+	if (personne) {
+		await personneRepository.deleteById(id);
 		return res
 			.sendStatus(204);
 	} else {
@@ -72,4 +71,26 @@ const remove = async (req, res, next) => {
 	}
 }
 
-export default { showAll, showOne, add, remove };
+const update = async (req, res, next) => {
+	if (req.params.id != req.body.id) {
+		return res.sendStatus(400)
+	}
+	if (!await personneRepository.findById(req.params.id)) {
+		return res.sendStatus(404)
+	}
+	personneSchema
+		.validate(req.body, { abortEarly: false })
+		.then(async () => {
+			await personneRepository.update(req.body)
+			return res
+				.status(202)
+				.json(req.body)
+		})
+		.catch(err => {
+			console.log(err);
+			return res
+				.sendStatus(500)
+		})
+}
+
+export default { showAll, showOne, add, remove, update };
--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-import express from 'express'
-import 'dotenv/config'
+import express from 'express';
+import 'dotenv/config';
+import auth from 'basic-auth';
+import personne from './routes/personne.route.js';
+import adresse from './routes/adresse.route.js';
 
-import personne from './routes/personne.route.js'
-import adresse from './routes/adresse.route.js'
+const admin = { username: "admin", password: "admin"}; // admin simulé (check db en vrai)
 
 // configurer yup
 
-const app = express()
+const app = express();
+
+// Configuration de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+})
+const basicAuth = (req, res, next) => {
+	// const userBase64 = req.headers['authorization'];
+	const unknownUser = auth(req);
+	const {name, pass} = unknownUser;
+	if (name != admin.username || pass != admin.password) {
+		return res.sendStatus(401);
+	}
+	// console.log(userBase64);
+	// console.log(unknownUser);
+	
+	next();
+}
+
+// app.use(basicAuth);
 
 // utiliser le middleware body-parser
-app.use(express.json())
+app.use(express.json());
 
 // configurer les ressources statiques
 // app.use(express.static('public'))
 
 
 // Mapping entre routes et le routeur
-app.use("/personnes", personne)
-app.use("/adresses", adresse)
+app.use("/personnes", basicAuth, personne);
+app.use("/adresses", adresse);
 
 
 
@@ -31,4 +55,4 @@ const PORT = process.env.PORT || 5555
 
 app.listen(PORT, () => {
     console.log(`Adresse serveur : http://localhost:${PORT}`);
-})
+});
